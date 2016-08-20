636a0ad4a0b99f8e1991dbf53139587be7f876e3
Manuel Salazar <m1_20@hotmail.com>
2016-08-20 10:02:37 -0500

    First working commit

---
+++ custom_title.php
@@ -0,0 +1,41 @@
+<?php
+//[node:field-tripulante-salud] Análisis: [node:field-fecha-analisis-salud:custom:d-m-Y]
+
+$nid = $node->nid;
+
+$query = db_select('node', 'n');
+$query->join('field_data_field_fecha_analisis_salud', 'f', 'f.entity_id = n.nid');
+$query->join('field_data_field_tripulante_salud', 's', 's.entity_id = n.nid');
+
+$query->join('node', 'n2', 'n2.nid = s.field_tripulante_salud_target_id');
+$query->join('field_data_field_nombre_personal', 'nom', 'nom.entity_id = n2.nid');
+$query->join('field_data_field_ap_paterno_personal', 'pat', 'pat.entity_id = n2.nid');
+$query->join('field_data_field_ap_materno_personal', 'mat', 'mat.entity_id = n2.nid');
+
+$query->addField('nom', 'field_nombre_personal_value', 'nombre');
+$query->addField('pat', 'field_ap_paterno_personal_value', 'paterno');
+$query->addField('mat', 'field_ap_materno_personal_value', 'materno');
+$query->addField('f', 'field_fecha_analisis_salud_value', 'fecha');
+
+ 
+$query->condition('n.type', 'salud', '=');
+$query->condition('n.nid', $nid, '=');
+$exeResults = $query->execute();
+$results = $exeResults->fetchAll();
+
+foreach ($results as $result) {
+  $raw_fecha = new DateTime($result->fecha);
+  $fecha = $raw_fecha->format('d-m-Y');
+  $nombre = isset($result->nombre) ? $result->nombre : '';
+  $paterno = isset($result->paterno) ? $result->paterno : '';
+  $materno = isset($result->materno) ? $result->materno : '';
+  if (isset($raw_fecha)) {
+    $title = $nombre . ' ' . $paterno . ' ' . $materno . ' - ' . 'Análisis: ' . $fecha;
+  } else {
+    $title = $nombre . ' ' . $paterno . ' ' . $materno;
+  }
+}
+
+return $title;
+
+?>
