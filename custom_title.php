34d213f23253acfdd7ba940b75eb3b3517786599
Manuel Salazar <m1_20@hotmail.com>
2016-08-26 21:40:14 -0500

    Empty title fixed.
    Fixed Title when field "Fecha de Analysis" is empty. It only takes the names.

--- custom_title.php
+++ custom_title.php
@@ -2,15 +2,14 @@
 //[node:field-tripulante-salud] Análisis: [node:field-fecha-analisis-salud:custom:d-m-Y]
 
 $nid = $node->nid;
-
 $query = db_select('node', 'n');
-$query->join('field_data_field_fecha_analisis_salud', 'f', 'f.entity_id = n.nid');
-$query->join('field_data_field_tripulante_salud', 's', 's.entity_id = n.nid');
+$query->leftJoin('field_data_field_fecha_analisis_salud', 'f', 'f.entity_id = n.nid');
+$query->leftJoin('field_data_field_tripulante_salud', 's', 's.entity_id = n.nid');
 
-$query->join('node', 'n2', 'n2.nid = s.field_tripulante_salud_target_id');
-$query->join('field_data_field_nombre_personal', 'nom', 'nom.entity_id = n2.nid');
-$query->join('field_data_field_ap_paterno_personal', 'pat', 'pat.entity_id = n2.nid');
-$query->join('field_data_field_ap_materno_personal', 'mat', 'mat.entity_id = n2.nid');
+$query->leftJoin('node', 'n2', 'n2.nid = s.field_tripulante_salud_target_id');
+$query->leftJoin('field_data_field_nombre_personal', 'nom', 'nom.entity_id = n2.nid');
+$query->leftJoin('field_data_field_ap_paterno_personal', 'pat', 'pat.entity_id = n2.nid');
+$query->leftJoin('field_data_field_ap_materno_personal', 'mat', 'mat.entity_id = n2.nid');
 
 $query->addField('nom', 'field_nombre_personal_value', 'nombre');
 $query->addField('pat', 'field_ap_paterno_personal_value', 'paterno');
@@ -24,12 +23,13 @@ $exeResults = $query->execute();
 $results = $exeResults->fetchAll();
 
 foreach ($results as $result) {
-  $raw_fecha = new DateTime($result->fecha);
-  $fecha = $raw_fecha->format('d-m-Y');
   $nombre = isset($result->nombre) ? $result->nombre : '';
   $paterno = isset($result->paterno) ? $result->paterno : '';
   $materno = isset($result->materno) ? $result->materno : '';
-  if (isset($raw_fecha)) {
+  
+  if (isset($result->fecha)) {
+    $raw_fecha = new DateTime($result->fecha);
+    $fecha = $raw_fecha->format('d-m-Y');
     $title = $nombre . ' ' . $paterno . ' ' . $materno . ' - ' . 'Análisis: ' . $fecha;
   } else {
     $title = $nombre . ' ' . $paterno . ' ' . $materno;
